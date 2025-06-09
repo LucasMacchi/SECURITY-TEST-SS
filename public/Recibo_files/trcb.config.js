8556e44c43f1f4c36b0fc09b0d727fd69780bd89
LucasMacchi <lucasmacchi25@gmail.com>
2025-06-09 10:14:48 -0300

    first

---
+++ public/Recibo_files/trcb.config.js
@@ -0,0 +1,19 @@
+;(function(win) {
+  win["trcb"] = {
+    baseURL: "/e/",
+    apiURL: "http://localhost:4200",
+    onboardingURL: "https://www.turecibo.com.ar/s",
+    adminURL: "https://admin.turecibo.com",
+    eformsURL: "https://eforms.turecibo.com",
+    eformsPORT: "443",
+    brandingURL: "https://branding.turecibo.com",
+    publicDevURL: "https://publics.turecibo.com",
+    recaptchaKey: "6LclsNgUAAAAAOA8Nldcf-1Hdlo41A5ZGdV3liZz",
+    customLoginURL: "https://cdn.turecibo.com/css/custom_json",
+    flowPaperKey: "1d2f0a65672ab42e29d",
+    flowPaperArKey: "833f949545f6e343eb3",
+    clarityClientId: "hzb75qr5dc",
+    honeyPot: "u3gsdidl17.execute-api.eu-west-1.amazonaws.com/ProdStage",
+    wootricClientId: "NPS-EU-6145bbf6",
+  }
+})(window)
